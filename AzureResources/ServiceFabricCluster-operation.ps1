535a194627b936c547db7b5adf5abf1dfd2cad93
David Chen <xchen178@hotmail.com>
2018-06-27 21:08:50 -0400

    new parameters

---
+++ AzureResources/ServiceFabricCluster-operation.ps1
@@ -0,0 +1,15 @@
+#
+# ServiceFabricCluster_operation.ps1
+#
+
+$ConnectArgs = @{  ConnectionEndpoint = 'nststeuagfapi.eastus.cloudapp.azure.com:19000';  X509Credential = $True;  StoreLocation = 'CurrentUser';  StoreName = "MY";   FindType = 'FindByThumbprint';  FindValue = "259BED11340E6E93B2374A32B388F9C501D56A28"   }
+Connect-ServiceFabricCluster @ConnectArgs
+
+$vmss=Get-AzureRmVmss -ResourceGroupName 'nststeuagfrgsfc' -VMScaleSetName 'mynt1vm'
+
+Remove-ServiceFabricNodeState -NodeName '_mynt1vm_10' -Force
+
+Remove-ServiceFabricNodeState -NodeName '_mynt1vm_11' -Force
+
+Remove-ServiceFabricNodeState -NodeName '_mynt1vm_12' -Force
+Remove-ServiceFabricNodeState -NodeName '_mynt1vm_13' -Force
